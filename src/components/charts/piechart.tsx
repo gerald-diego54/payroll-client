1e51905e3fac92dc535840b83e42404e8bfa9c88
Gerald <90550820+gerald-diego54@users.noreply.github.com>
2023-10-24 21:48:37 +0800

    created reusable piechart

--- src/components/charts/piechart.tsx
+++ src/components/charts/piechart.tsx
@@ -1,38 +1,32 @@
-import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import React from 'react';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import React from "react";
+import { Stack, Typography, Paper, Box } from "@mui/material";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-export const data = {
-datasets: [
-    {
-      label: 'Percentage of Employees by Gender',
-      data: [70,30],
-      backgroundColor: [
-        '#EAF6ED',
-        '#67C587',
-      ],
-      borderColor: [
-        '#EAF6ED',
-        '#67C587',
-      ],
-      borderWidth: 1,
-    },   
-  ],
-  labels: ['Male','Female'],
+ChartJS.defaults.color = "#ffffff";
+
+const Piechart: React.FC<{ title: string; data: any; options: any }> = ({ title, data, options }): JSX.Element => {
+    return (
+        <Paper
+            sx={{
+                position: "relative",
+                backgroundColor: "#044453",
+                width: 381,
+                height: 419,
+            }}
+            elevation={4}
+        >
+            <Typography align="center" sx={{ color: "#ffffff", paddingTop: 3 }}>
+                {title}
+            </Typography>
+            <Stack direction="row" justifyContent="center" alignItems="center" sx={{ width: 1, height: "80%" }}>
+                <Box sx={{ width: 250, height: 250, margin: "auto" }}>
+                    <Pie options={options} data={data} />
+                </Box>
+            </Stack>
+        </Paper>
+    );
 };
 
-export function Piechart() {
-    return (
-    <Paper sx={{position: "relative", backgroundColor: '#044453', width: '381px', height: '419px'}}>
-    <Box sx={{width: '200px', height: '200px', margin: "auto",}}>
-    <Pie
-    options={{ maintainAspectRatio: true }}
-    data={data} 
-    />
-    </Box>
-    </Paper>
-    )
-}
+export default Piechart;
