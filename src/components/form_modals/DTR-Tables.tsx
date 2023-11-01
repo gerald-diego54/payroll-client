78e2ed6fcc318742424a7dc7d9217c0694ca069d
Nero <magnusalvarice@gmail.com>
2023-11-01 18:13:14 +0800

    DTR Year Drop Down

--- src/components/form_modals/DTR-Tables.tsx
+++ src/components/form_modals/DTR-Tables.tsx
@@ -9,9 +9,10 @@ import Paper from "@mui/material/Paper";
 import Checkbox from "@mui/material/Checkbox";
 import EditButton from "@/src/ui/Edit";
 import DeleteButton from "@/src/ui/DeleteButton";
-import { DtrData} from "@/data/DTR_data";
+import { DtrData } from "@/data/DTR_data";
 import BasicDatePicker from "@/src/ui/DatePicker";
 import DropdownMenu from "@/src/layout/DtrSelectionMenu";
+import YearDropdown from "@/src/ui/DTR-YearDrop";
 
 const cellStyle = {
   fontSize: 18,
@@ -24,13 +25,21 @@ const dataCellStyle = {
   fontSize: 14,
   fontWeight: 500,
   border: "solid 2px black",
+  backgroundColor: "white",
+};
+
+const tableContainerStyle = {
+  border: "solid 3px black",
+  padding: "1rem", // Adjust the padding as needed
+  backgroundColor: "#a6e9b8",
 };
 
 const DTR_Tables = () => {
   const rows: any[] = DtrData;
-  
+
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} sx={tableContainerStyle}>
+      <YearDropdown />
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
@@ -66,10 +75,18 @@ const DTR_Tables = () => {
               key={index}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row" align="center">
+              <TableCell
+                component="th"
+                scope="row"
+                align="center"
+                style={{ backgroundColor: "#fff" }}
+              >
                 <EditButton />
               </TableCell>
-              <TableCell align="center" style={{ border: "solid black 2px" }}>
+              <TableCell
+                align="center"
+                style={{ border: "solid black 2px", backgroundColor: "#fff" }}
+              >
                 <DeleteButton />
               </TableCell>
               <TableCell align="center" style={dataCellStyle}>
@@ -78,14 +95,14 @@ const DTR_Tables = () => {
               <TableCell align="center" style={dataCellStyle}>
                 {row.year}
               </TableCell>
-              <BasicDatePicker />
               <TableCell align="center" style={dataCellStyle}>
-              <DropdownMenu/>
+                <BasicDatePicker />
+              </TableCell>
+              <TableCell align="center" style={dataCellStyle}>
+                <DropdownMenu />
               </TableCell>
               <TableCell align="center" style={dataCellStyle}></TableCell>
-              <TableCell align="center" style={dataCellStyle}>
-              
-              </TableCell>
+              <TableCell align="center" style={dataCellStyle}></TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -95,10 +112,18 @@ const DTR_Tables = () => {
               key={index}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row" align="center">
+              <TableCell
+                component="th"
+                scope="row"
+                align="center"
+                style={{ backgroundColor: "#fff" }}
+              >
                 <EditButton />
               </TableCell>
-              <TableCell align="center" style={{ border: "solid black 2px" }}>
+              <TableCell
+                align="center"
+                style={{ border: "solid black 2px", backgroundColor: "#fff" }}
+              >
                 <DeleteButton />
               </TableCell>
               <TableCell align="center" style={dataCellStyle}>
@@ -107,21 +132,17 @@ const DTR_Tables = () => {
               <TableCell align="center" style={dataCellStyle}>
                 {row.year}
               </TableCell>
-              <BasicDatePicker />
               <TableCell align="center" style={dataCellStyle}>
-              <DropdownMenu/>
-              </TableCell>
-                  <TableCell align="center" style={dataCellStyle}>
-                      
+                <BasicDatePicker />
               </TableCell>
               <TableCell align="center" style={dataCellStyle}>
-               
+                <DropdownMenu />
               </TableCell>
+              <TableCell align="center" style={dataCellStyle}></TableCell>
+              <TableCell align="center" style={dataCellStyle}></TableCell>
             </TableRow>
           ))}
-              </TableBody>
-              
-              
+        </TableBody>
       </Table>
     </TableContainer>
   );
---
+++ src/ui/DTR-YearDrop.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import { Box } from "@mui/material";
+
+const YearDropdown = () => {
+  const [selectedYear, setSelectedYear] = useState("");
+
+  const years = Array.from(
+    { length: 10 },
+    (_, index) => new Date().getFullYear() - index
+  );
+
+  const handleChange = (event) => {
+    setSelectedYear(event.target.value);
+  };
+
+  const yearBox = {
+      width: "25%",
+      padding: 1,
+      marginLeft: 0,
+      display:"inline-flex",
+    backgroundColor: "#fff",
+      borderRadius:"10px",
+  };
+
+  return (
+    <Box sx={yearBox}>
+      <FormControl fullWidth>
+        <InputLabel htmlFor="year-select">Year</InputLabel>
+        <Select
+          value={selectedYear}
+          onChange={handleChange}
+          label="Year"
+          inputProps={{
+            id: "year-select",
+          }}
+        >
+          {years.map((year) => (
+            <MenuItem key={year} value={year}>
+              {year}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default YearDropdown;
