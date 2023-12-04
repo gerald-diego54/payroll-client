6cdde27b6de65a3175d889e2a6a14fd6878f7ef7
Akuyakoaizen <130422785+Akuyakoaizen@users.noreply.github.com>
2023-12-04 23:22:40 +0800

    Log out functionality

--- src/environment/connection.ts
+++ src/environment/connection.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-export const connection = axios.create({
+const connection = axios.create({
     baseURL: "http://localhost:8000/api/",
     headers: {
         "Content-Type": "application/json",
     },
 });
+
+export { connection };
